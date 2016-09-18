15b10e5b51b4ed40f7b74c7c4d5e79c9fd05a6c9
Bertrand Martel <bmartel.fr@gmail.com>
2016-09-18 05:14:58 +0200

    configure timezone + ssh directory

--- vars-template.sh
+++ vars-template.sh
@@ -3,3 +3,5 @@ export HTTPS_INTERNAL_PORT=8083
 export KEYSTORE_FILE_PATH=/home/bobby/keys/keystore.jks
 export KEYSTORE_PWD=keystore_pass
 export JENKINS_HOME_PATH=/home/bobby/jenkins_home
+export SSH_HOME=/home/jenkins/.ssh
+export TIMEZONE=CET
