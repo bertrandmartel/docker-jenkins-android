8cfa712066a4496d96c87f406a5c17fd48d86b3f
Bertrand Martel <bmartel.fr@gmail.com>
2016-10-05 23:01:19 +0200

    setup configurable Android SDK & NDK if not existing

--- Dockerfile
+++ Dockerfile
@@ -4,18 +4,24 @@ MAINTAINER Bertrand Martel <bmartel.fr@gmail.com>
 
 USER root
 
-# install lib32stdc++6 lib32z1 for aapt
-RUN apt-get update && apt-get install -y lib32stdc++6 lib32z1
+# install lib32stdc++6 lib32z1 for aapt && build-essential and file for ndk
+RUN apt-get update && apt-get install -y lib32stdc++6 lib32z1 build-essential file
 
 COPY docker-entrypoint.sh /
 COPY run.sh /
 
+ENV NDK_PATH "/opt/android/ndk"
+ENV SDK_PATH "/opt/android/sdk"
+
 RUN chmod 777 /docker-entrypoint.sh
 RUN chmod 777 /run.sh
 
-RUN /usr/local/bin/install-plugins.sh gitlab-plugin:1.4.0 android-emulator:2.15 gitlab-logo:1.0.1 gitlab-oauth:1.0.8 ws-cleanup:0.30 slack:2.0.1 embeddable-build-status:1.9
+RUN /usr/local/bin/install-plugins.sh gitlab-plugin:1.4.0 android-emulator:2.15 gitlab-logo:1.0.1 gitlab-oauth:1.0.8 ws-cleanup:0.30 slack:2.0.1 embeddable-build-status:1.9 gradle:1.25
 
 COPY ./gitlab-oauth.hpi /usr/share/jenkins/ref/plugins/gitlab-oauth.jpi
+COPY ./jenkins.sh /usr/local/bin/jenkins.sh
+
+RUN chmod 777 /usr/local/bin/jenkins.sh
 
 ENTRYPOINT ["/docker-entrypoint.sh"]
 CMD ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,5 +1,50 @@
 #!/bin/bash
 
+mkdir -p /var/jenkins_home/.android
+
 chown -R jenkins:jenkins /var/jenkins_home
 
-chroot --userspec=jenkins / /bin/tini -- /usr/local/bin/jenkins.sh
+if [ -z "$VERSION_ANDROID_NDK" ]; then
+	export VERSION_ANDROID_NDK=android-ndk-r12b
+fi
+
+if [ -z "$VERSION_ANDROID_SDK" ]; then
+	export VERSION_ANDROID_SDK=r25.2.2
+fi
+
+export ANDROID_NDK_HOME=${NDK_PATH}/${VERSION_ANDROID_NDK}
+export PATH=$PATH:$ANDROID_NDK_HOME:$SDK_PATH/tools
+export ANDROID_HOME=$SDK_PATH
+
+# check if NDK is present
+if hash ndk-build 2>/dev/null; then
+    echo "Android NDK already exists"
+else
+	mkdir -p ${NDK_PATH}
+	echo "downloading NDK..."
+	wget --no-verbose https://dl.google.com/android/repository/${VERSION_ANDROID_NDK}-linux-x86_64.zip -O ${NDK_PATH}/ndk.zip
+	unzip ${NDK_PATH}/ndk.zip -d ${NDK_PATH} && rm ${NDK_PATH}/ndk.zip
+	echo "Android NDK has been installed to ${NDK_PATH}"
+fi
+
+# check if SDK is present
+if hash android 2>/dev/null; then
+    echo "Android SDK already exists"
+else
+	mkdir -p ${SDK_PATH}
+	echo "downloading SDK..."
+	wget --no-verbose https://dl.google.com/android/repository/tools_${VERSION_ANDROID_SDK}-linux.zip -O ${SDK_PATH}/sdk.zip
+	unzip ${SDK_PATH}/sdk.zip -d ${SDK_PATH} && rm ${SDK_PATH}/sdk.zip
+	echo "Android SDK has been installed to ${NDK_PATH}"
+fi
+
+echo y | android update sdk --no-ui
+
+if [ ! -z "$ANDROID_SDK_FILTER" ]; then
+	echo y | android update sdk -a --filter $ANDROID_SDK_FILTER --no-ui
+fi
+
+chown -R jenkins:jenkins $NDK_PATH
+chown -R jenkins:jenkins $SDK_PATH
+
+chroot --userspec=jenkins / /bin/tini -- /usr/local/bin/jenkins.sh $ANDROID_HOME $ANDROID_NDK_HOME 
