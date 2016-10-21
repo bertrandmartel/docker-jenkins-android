c514b12205996912f53609ff5760d5d062637256
Bertrand Martel <bmartel.fr@gmail.com>
2016-10-21 17:35:41 +0200

    add SDK license configuration

--- install.sh
+++ install.sh
@@ -51,6 +51,10 @@ else
 	echo "Android SDK has been installed to ${NDK_PATH}"
 fi
 
+mkdir -p "$SDK_PATH/licenses" || true
+echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "$SDK_PATH/licenses/android-sdk-license"
+echo -e "\n84831b9409646a918e30573bab4c9c91346d8abd" > "$SDK_PATH/licenses/android-sdk-preview-license"
+
 echo y | android update sdk --no-ui
 
 if [ ! -z "$ANDROID_BUILD_TOOLS_FILTER" ]; then
